aa9f73a31c139fc916ae2fbf7fee6405a9f69f9c
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-13 14:50:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,8 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date: 1990} )
   # release_date == 1990
 
   Movie.create do |m|
-  m = arg
+  m.title = arg[:title)
+  m.release_date = arg[:release_date]
   end
 end
 
