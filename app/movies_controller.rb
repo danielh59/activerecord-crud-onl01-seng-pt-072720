a9b78be4e6925e0f22382dce6c59665e537582af
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-13 15:04:16 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -85,7 +85,7 @@ end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
-  if Movie.find_by(name: "A Movie")
+  if Movie.find_by(title: "A Movie")
 
 else
     Movie.update(title: "A Movie")
